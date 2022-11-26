08df7c6c8747bf21ff8c1c28808a661f07b10f52
lwq <2065277594@qq.com>
2022-11-26 23:29:58 +0800

    Redis实现分布式锁

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -301,59 +301,49 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
     }*/
-    /*@Transactional
-    public Result createVoucherOrder(Long voucherId) {
-        // 5.一人一单
-        Long userId = UserHolder.getUser().getId();
 
-        // 创建锁对象
-        SimpleRedisLock redisLock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
-        // 尝试获取锁
-        boolean isLock = redisLock.tryLock(1200);
-        // 判断
-        if(!isLock){
-            // 获取锁失败，直接返回失败或者重试
-            return Result.fail("不允许重复下单！");
-        }
-
-        try {
-            // 5.1.查询订单
-            int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
-            // 5.2.判断是否存在
-            if (count > 0) {
-                // 用户已经购买过了
-                return Result.fail("用户已经购买过一次！");
-            }
-
-            // 6.扣减库存
-            boolean success = seckillVoucherService.update()
-                    .setSql("stock = stock - 1") // set stock = stock - 1
-                    .eq("voucher_id", voucherId).gt("stock", 0) // where id = ? and stock > 0
-                    .update();
-            if (!success) {
-                // 扣减失败
-                return Result.fail("库存不足！");
-            }
-
-            // 7.创建订单
-            VoucherOrder voucherOrder = new VoucherOrder();
-            // 7.1.订单id
-            long orderId = redisIdWorker.nextId("order");
-            voucherOrder.setId(orderId);
-            // 7.2.用户id
-            voucherOrder.setUserId(userId);
-            // 7.3.代金券id
-            voucherOrder.setVoucherId(voucherId);
-            save(voucherOrder);
-
-            // 7.返回订单id
-            return Result.ok(orderId);
-        } finally {
-            // 释放锁
-            redisLock.unlock();
-        }
-
-    }*/
+//    //3.redis实现分布式锁
+//    @Transactional
+//    public Result createVoucherOrder(Long voucherId) {
+//        Long userId = UserHolder.getUser().getId();
+//
+//        // 创建锁对象
+//        SimpleRedisLock redisLock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
+//        // 尝试获取锁
+//        boolean isLock = redisLock.tryLock(1200);
+//        // 判断
+//        if (!isLock) {
+//            //根据业务决定是直接返回错误信息还是重试
+//            return Result.fail("不允许重复下单！");
+//        }
+//
+//        try {
+//            // 5.1.查询订单
+//            int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+//            if (count > 0) {
+//                return Result.fail("用户已经购买过一次！");
+//            }
+//            // 6.扣减库存
+//            boolean success = seckillVoucherService.update()
+//                    .setSql("stock = stock - 1") // set stock = stock - 1
+//                    .eq("voucher_id", voucherId).gt("stock", 0) // where id = ? and stock > 0
+//                    .update();
+//            if (!success) {
+//                return Result.fail("库存不足！");
+//            }
+//            // 7.创建订单返回id
+//            VoucherOrder voucherOrder = new VoucherOrder();
+//            long orderId = redisIdWorker.nextId("order");
+//            voucherOrder.setId(orderId);
+//            voucherOrder.setUserId(userId);
+//            voucherOrder.setVoucherId(voucherId);
+//            save(voucherOrder);
+//            return Result.ok(orderId);
+//        } finally {
+//            // 释放锁
+//            redisLock.unlock();
+//        }
+//    }
 
 
 //    //**************************************************************2.下单操作(单机情况下)
--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -8,6 +8,9 @@ import org.springframework.data.redis.core.script.DefaultRedisScript;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 分布式锁
+ */
 public class SimpleRedisLock implements ILock {
 
     private String name;
@@ -18,25 +21,55 @@ public class SimpleRedisLock implements ILock {
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    //k的前缀标识
     private static final String KEY_PREFIX = "lock:";
-    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    //v的前缀标识
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-"; //设为true去掉uuid里的‘-’
+
+
+    /**
+     * lua脚本
+     */
     private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
     static {
         UNLOCK_SCRIPT = new DefaultRedisScript<>();
         UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
         UNLOCK_SCRIPT.setResultType(Long.class);
     }
 
+    /**
+     * 尝试获取锁
+     */
     @Override
     public boolean tryLock(long timeoutSec) {
-        // 获取线程标示
+        // 获取线程标示作为v
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
                 .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
-        return Boolean.TRUE.equals(success);
+        return Boolean.TRUE.equals(success);    //避免返回值是null拆箱出现空指针
     }
 
+//    /**
+//     * 释放锁：要避免因为获取锁之后业务执行实现太长超过锁的过期时间，然后其他线程获取锁，而本线程业务完毕后释放的将是别人的锁
+//     */
+//    @Override
+//    public void unlock() {
+//        // 获取线程标示
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        // 获取锁中的标示
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        // 判断标示是否一致
+//        if (threadId.equals(id)) {
+//            // 再释放锁，防止释放别人的锁 （注意，判断与真正释放不是原子操作，依旧有问题，因为是分布式的加synchronized也没用，应该使用redis的‘事务’）
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
+
+    /**
+     * 释放锁2.0，利用lua脚本原子的执行redis操作
+     */
     @Override
     public void unlock() {
         // 调用lua脚本
@@ -45,16 +78,5 @@ public class SimpleRedisLock implements ILock {
                 Collections.singletonList(KEY_PREFIX + name),
                 ID_PREFIX + Thread.currentThread().getId());
     }
-    /*@Override
-    public void unlock() {
-        // 获取线程标示
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 获取锁中的标示
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        // 判断标示是否一致
-        if(threadId.equals(id)) {
-            // 释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
-    }*/
+
 }
